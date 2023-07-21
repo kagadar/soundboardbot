38e44791d6ec85d12072fbef58f245bc9bb32e44
Harrison Short <kagadar@gmail.com>
2023-07-21 10:23:00 +1000

    Use the `server_suffix` option in guild creation

--- helpers.go
+++ helpers.go
@@ -1,33 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/bwmarrin/discordgo"
-)
-
-type token struct{}
-
-func stringPtr(x string) *string {
+func toPtr[T any](x T) *T {
 	return &x
-}
-
-func interactionUser(event *discordgo.InteractionCreate) (*discordgo.User, error) {
-	var user *discordgo.User
-	if event.User != nil {
-		user = event.User
-	} else {
-		if event.Member == nil {
-			return nil, fmt.Errorf("interaction request recevied without any identified user: %+v", event)
-		}
-		user = event.Member.User
-	}
-	return user, nil
-}
-
-func username(u *discordgo.User) string {
-	if u.Discriminator != "0" {
-		return fmt.Sprintf("%s#%s", u.Username, u.Discriminator)
-	}
-	return u.Username
 }
